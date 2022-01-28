ae36593fac59fef5e5aac312782e71f98fb23868
tyckinas <klikunastitas@gmail.com>
2022-01-28 14:49:39 +0200

    feat: db connection established , env vars added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const mysql = require('mysql')
+require('dotenv').config()
+const express = require("express");
+const mysql = require("mysql");
+
+
+//Create connection
+const db = mysql.createConnection({
+    host     : `${process.env.DB_HOST}`,
+    user     : `${process.env.DB_USER}`,
+    password : `${process.env.DB_PASSWORD}`,
+    database : `${process.env.DB}`
+});
+
+db.connect((err) =>{
+    if(err) {
+        throw err;
+    }
+    console.log('MySQL connected')
+})
 
 const server = express();
 
-server.listen('3000' , ()=>{
-    console.log('Server is running')
-})
+server.listen(`${process.env.SERVER_PORT}`, () => {
+  console.log("Server is running");
+});
